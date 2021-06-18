1358afade020d91eaa0185dacd27df3634f26cc8
Harithaballa <82507410+Harithaballa@users.noreply.github.com>
2021-06-18 21:50:35 +0530

    First Comment

---
+++ APIForMciroService/src/main/java/com/redshift/APIForMciro/ApiForMciroServiceApplication.java
@@ -0,0 +1,15 @@
+package com.redshift.APIForMciro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ApiForMciroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForMciroServiceApplication.class, args);
+	}
+
+}
